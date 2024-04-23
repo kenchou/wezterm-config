d2eeaa461f7ae91388f41902bd121ea0c4347480
Ken Chou <zhang-j@klab.com>
2024-04-23 10:58:09 +0800

    cmd+click to open link

--- wezterm.lua
+++ wezterm.lua
@@ -198,13 +198,13 @@ config.mouse_bindings = {
   -- Bind 'Up' event of CTRL-Click to open hyperlinks
   {
     event = { Up = { streak = 1, button = 'Left' } },
-    mods = 'CTRL',
+    mods = 'CMD',
     action = wezterm.action.OpenLinkAtMouseCursor,
   },
   -- Disable the 'Down' event of CTRL-Click to avoid weird program behaviors
   {
     event = { Down = { streak = 1, button = 'Left' } },
-    mods = 'CTRL',
+    mods = 'CMD',
     action = wezterm.action.Nop,
   },
 }
